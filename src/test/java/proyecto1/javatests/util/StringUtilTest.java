971c0ee09148dc6538b0f510c8aeb322e9d7c114
Jose Ramon <joseramonnavarrogonzalez402@gmail.com>
2021-06-16 01:26:56 -0500

    tercera parte

--- src/test/java/proyecto1/javatests/util/StringUtilTest.java
+++ src/test/java/proyecto1/javatests/util/StringUtilTest.java
@@ -1,19 +1,15 @@
 package proyecto1.javatests.util;
 
+import org.junit.Assert;
 public class StringUtilTest {
 
-    public static void main(String[] args) {
+    @Test
+    public void testRepeat{
 
-        assertEquals(StringUtil.repeat("hola", 3),"holaholahola");
+        Assert.assertEquals("holaholahola", StringUtil.repeat("hola", 3));
 
 
-        assertEquals(StringUtil.repeat("hola", 1),"hola");
+        Assert.assertEquals("hola"StringUtil.repeat("hola", 1));
     }
 
-    private static void assertEquals(String actual, String expected) {
-        if(!actual.equals(expected)){
-            throw new RuntimeException(actual + " is not equal to expected" + expected);
-
-        }
-    }
 }
